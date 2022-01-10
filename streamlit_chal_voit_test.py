3c7da0e18787e3506999211e7a4a93acd45d8f62
JOLYfranck <franck.joly10@gmail.com>
2022-01-10 17:42:31 +0100

    Update streamlit_chal_voit_test.py

--- streamlit_chal_voit_test.py
+++ streamlit_chal_voit_test.py
@@ -109,7 +109,7 @@ def analyse_Us():
 
 def analyse_Japon():
     dj_japon = (df[df['continent'].str.contains("Japan.")])
-    st.write (dj_japon.sort_values(by='cubicinches',ascending=False))
+    st.write (dj_japon.sort_values(by='MPG',ascending=True))
 
     st.write ('-----------------------------------------------------------')
 
